bbd0eeb2b23cf863cddadf307c2f3d4a7ecdae31
SantiRendon <xantirendonc@gmail.com>
2024-06-28 19:03:15 -0500

    feat: Add Navbar component for website navigation
    This commit adds a new component called "Navbar.svelte" to the project. The Navbar component is responsible for displaying the navigation menu on the website. It includes a list of menu items such as "Acerca", "Ayuda", and "Empieza un Proyecto". The component also imports and uses the ConnectPlug component for wallet connection. Some commented out code and styles have been removed from the Navbar.svelte file.

---
+++ src/ecofund-icp-frontend/src/lib/Navbar.svelte
@@ -0,0 +1,52 @@
+<script>
+    // import { logoBurbujas } from "../../static/logoBurbujas.png";
+    import ConnectPlug from "$lib/ConnectPlug.svelte";
+</script>
+
+<header>
+    <nav>
+        <!-- <img src={logoBurbujas} class="logo" alt="Logo de Burbujas" /> -->
+
+        <ul>
+            <li>Acerca</li>
+            <li>Ayuda</li>
+            <li>Empieza un Proyecto</li>
+        </ul>
+    </nav>
+    <ConnectPlug/>
+    <!-- <button id="button_Nav">Login with ICP</button> -->
+</header>
+
+<style>
+    header {
+        display: flex;
+        padding: 0;
+    }
+
+    nav {
+        width: 100%;
+        display: flex;
+        padding: 0 2em 0 2em;
+        justify-content: space-evenly;
+    }
+
+    ul {
+        display: flex;
+        list-style: none;
+    }
+
+    ul li {
+        padding: 1.5rem;
+        font-family: "Courier New", Courier, monospace;
+    }
+
+    /* #button_Nav {
+        width: 250px;
+        background-color: rgb(49, 192, 49);
+        border-radius: 15px;
+        border: 0;
+        font-size: 15px;
+        color: #ffffff;
+        float: right;
+    } */
+</style>
