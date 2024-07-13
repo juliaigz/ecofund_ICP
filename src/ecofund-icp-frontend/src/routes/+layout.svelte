ce9c84890874ec747cb78a431c69bab419c6e47e
SantiRendon <xantirendonc@gmail.com>
2024-07-13 01:26:13 -0500

    feat: Add carbon-components-svelte for layout styling
    This commit adds the `carbon-components-svelte` library to the project, specifically for the `+layout.svelte` file. The library provides pre-styled components and CSS classes that can be used to enhance the layout and styling of the application. This addition will improve the visual appearance and user experience of the application's layout.

--- src/ecofund-icp-frontend/src/routes/+layout.svelte
+++ src/ecofund-icp-frontend/src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script>
+    import "carbon-components-svelte/css/white.css"; // https://github.com/carbon-design-system/carbon-components-svelte?tab=readme-ov-file
     import Navbar from "$lib/Navbar.svelte";
 </script>
 
---
+++ src/ecofund-icp-frontend/src/routes/carbon/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { AspectRatio } from "carbon-components-svelte";
+  import { Airplane, AiGovernanceModel } from "carbon-pictograms-svelte";
+  import { Ai } from "carbon-icons-svelte";
+</script>
+
+<!-- <Button>Primary button</Button> -->
+<AspectRatio>
+
+  <Airplane />
+  <br />
+  <AiGovernanceModel />
+  <Ai />
+</AspectRatio>
+
+<!-- {JSON.stringify(breakpoints)} -->
