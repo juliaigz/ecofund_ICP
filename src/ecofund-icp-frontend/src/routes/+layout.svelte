3f644a0760238881ee58331e6bc021f11644593c
SantiRendon <xantirendonc@gmail.com>
2024-07-16 14:15:53 -0500

    finish config

--- src/ecofund-icp-frontend/src/routes/+layout.svelte
+++ src/ecofund-icp-frontend/src/routes/+layout.svelte
@@ -1,7 +1,58 @@
-<script>
+<!-- <script>
     import Navbar from "$lib/Navbar.svelte";
 </script>
 
-<Navbar />
+<Navbar /> -->
+
+<script lang="ts">
+  import TopAppBar, { Row, Section, Title } from "@smui/top-app-bar";
+  import IconButton from "@smui/icon-button";
+  import Checkbox from "@smui/checkbox";
+  import FormField from "@smui/form-field";
+
+  let prominent = false;
+  let dense = false;
+  let secondaryColor = false;
+</script>
+
+<TopAppBar
+  variant="static"
+  {prominent}
+  {dense}
+  color={secondaryColor ? "secondary" : "primary"}
+>
+  <Row>
+    <Section>
+      <IconButton class="material-icons">menu</IconButton>
+      <Title>Static</Title>
+    </Section>
+    <Section align="end" toolbar>
+      <IconButton class="material-icons" aria-label="Download"
+        >file_download</IconButton
+      >
+      <IconButton class="material-icons" aria-label="Print this page"
+        >print</IconButton
+      >
+      <IconButton class="material-icons" aria-label="Bookmark this page"
+        >bookmark</IconButton
+      >
+    </Section>
+  </Row>
+</TopAppBar>
+
+<div>
+  <FormField>
+    <Checkbox bind:checked={prominent} />
+    <span slot="label">Prominent</span>
+  </FormField>
+  <FormField>
+    <Checkbox bind:checked={dense} />
+    <span slot="label">Dense</span>
+  </FormField>
+  <FormField>
+    <Checkbox bind:checked={secondaryColor} />
+    <span slot="label">Secondary</span>
+  </FormField>
+</div>
 
 <slot />
