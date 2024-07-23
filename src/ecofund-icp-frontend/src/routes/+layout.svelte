08a598b777f1938acb8039aa86e878048df8f32f
SantiRendon <xantirendonc@gmail.com>
2024-07-22 23:49:29 -0500

    refactor: Update +layout.svelte to use carbon-components-svelte for styling
    This commit updates the +layout.svelte file to use the carbon-components-svelte library for styling. It includes the necessary CSS classes and components to enhance the visual appearance and user experience of the application's layout. This change improves the overall design consistency and usability of the application.

--- src/ecofund-icp-frontend/src/routes/+layout.svelte
+++ src/ecofund-icp-frontend/src/routes/+layout.svelte
@@ -1,15 +1,10 @@
 <script>
-    import "carbon-components-svelte/css/all.css"; // https://github.com/carbon-design-system/carbon-components-svelte?tab=readme-ov-file
-    import Navbar from "$lib/Navbar.svelte";
-
+  import "carbon-components-svelte/css/all.css"; // https://github.com/carbon-design-system/carbon-components-svelte?tab=readme-ov-file
+  import Navbar from "$lib/Navbar.svelte";
 </script>
 
 <Navbar />
 <slot />
 
-
-
-
 <style>
-
 </style>
