b04872533d7650d5534b74eff6a6a84ba662d6a8
SantiRendon <xantirendonc@gmail.com>
2024-07-22 23:48:55 -0500

    refactor: Update Cards.svelte to use ImageLoader component for lazy loading images
    This commit updates the Cards.svelte file to use the ImageLoader component from the carbon-components-svelte library for lazy loading images. It replaces the direct image tags with the ImageLoader component, which improves page load performance by loading images only when they are visible on the screen. This change enhances the user experience by reducing the initial load time of the page.

--- src/ecofund-icp-frontend/src/lib/Cards.svelte
+++ src/ecofund-icp-frontend/src/lib/Cards.svelte
@@ -1,72 +1,49 @@
 <script>
-    import Sinba from "$lib/images/Sinba.jpg";
-    import ColombiaAval from "$lib/images/ColombiaAval.png";
-    import Reciclaje from "$lib/images/ Reciclaje.jpg";
-    import { Grid, Row, Column } from "carbon-components-svelte";
+  import Sinba from "$lib/images/Sinba.jpg";
+  import ColombiaAval from "$lib/images/ColombiaAval.png";
+  import Reciclaje from "$lib/images/ Reciclaje.jpg";
+  import { Grid, Row, Column, ImageLoader } from "carbon-components-svelte";
 
-
-    //*Importaciones de las Imagenes de Perfil sobre los apoyos
-    import apoyo1 from "$lib/images/apoyo1.png";
-    import apoyo2 from "$lib/images/apoyo2.png";
-
-
+  //*Importaciones de las Imagenes de Perfil sobre los apoyos
+  import apoyo1 from "$lib/images/apoyo1.png";
+  import apoyo2 from "$lib/images/apoyo2.png";
 </script>
 
 <Grid narrow>
-    <Row>
-      <Column>Food</Column>
-      <Column>Reciclyng</Column>
-      <Column>Animals</Column>
-      <Column>Agriculture</Column>
-      <Column>Header</Column>
-    </Row>
+  <Row>
+    <Column>Food</Column>
+    <Column>Reciclyng</Column>
+    <Column>Animals</Column>
+    <Column>Agriculture</Column>
+    <Column>Header</Column>
+  </Row>
 </Grid>
-<hr>
+<hr />
 <!--*CONTENEDOR SOBRE LA INFORMACIÓN SOBRE LAS INVERSIONES-->
-<Grid narrow style="display:flex; justify-content: space-around; "> <!--Primera Columna-->
+<Grid narrow style="display:flex; justify-content: space-around; ">
+  <Row
+    class={"class"}
+    style="background-color: gray; width:14rem; height:68vh; display: flex; flex-direction: column; align-items: center;"
+  >
+    <ImageLoader src={Sinba} alt="Logo de la campaña de reciclaje" />
+    <Column style="text-align: center;">Sinba.ICP</Column>
+  </Row>
 
-    <Row class={"class"} style="background-color: gray; width:14rem; height:68vh; display: flex; flex-direction: column; align-items: center;">
-        <img src={Sinba} alt="Logo de la campaña de reciclaje">
-      <Column style="text-align: center;">Sinba.ICP</Column>
-    </Row>
+  <!-- <Row
+    style="background-color: gray; width: 14rem; height: 68vh; display: felx; flex-direction:column; align-items: center;"
+  >
+    <ImageLoader src={ColombiaAval} alt="Loco de compaña sobre inversión en Colombia" />
 
+    <div class="ImgYNombreUsuario">
+      <ImageLoader src={apoyo2} alt="Imagen de perfil sobre Sinba" />
+      <Column>Luis.ICP</Column>
+    </div>
+  </Row>
 
-    <!--Tarjeta 2-->
-    <Row style="background-color: gray; width: 14rem; height: 68vh; display: felx; flex-direction:column; align-items: center;">
-        <img src="{ColombiaAval}" alt="Loco de compaña sobre inversión en Colombia">
-        
-        <div class="ImgYNombreUsuario">
-            <img src={apoyo2} alt="Imagen de perfil sobre Sinba">
-            <Column>Luis.ICP</Column>
-        </div>
-    </Row>
-    
-    <!--Tarjeta 3-->
-
-    <Row>
-        <Column>Tarjeta 3</Column>
-    </Row>
-
+  <Row>
+    <Column>Tarjeta 3</Column>
+  </Row> -->
 </Grid>
-
-
 
 <style>
-   hr{
-    width: 95%;
-   } 
-
-   img{
-    object-fit: cover;
-    width: 25%;
-    grid: 23rem;
-   }
-
-   .ImgYNombreUsuario{
-    display: flex;
-    justify-content: center;
-   }
-  
-
-   
 </style>
