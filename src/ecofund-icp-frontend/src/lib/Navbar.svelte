7415b7f6c8353e91032358010b6337ffb5d74b95
SantiRendon <xantirendonc@gmail.com>
2024-07-22 23:48:46 -0500

    refactor: Update Navbar component to include Cards and remove unused imports
    This commit refactors the Navbar component in the `Navbar.svelte` file. It includes the Cards component and removes unused imports for improved code organization and performance.

--- src/ecofund-icp-frontend/src/lib/Navbar.svelte
+++ src/ecofund-icp-frontend/src/lib/Navbar.svelte
@@ -1,8 +1,9 @@
 <script>
-    // import { logoBurbujas } from "../../static/logoBurbujas.png";
-    import ConnectPlug from "$lib/ConnectPlug.svelte";
-    import TransferPlug from "./TransferPlug.svelte";
-    import {
+  // import { logoBurbujas } from "../../static/logoBurbujas.png";
+  import ConnectPlug from "$lib/ConnectPlug.svelte";
+  import Cards from "./Cards.svelte";
+  import TransferPlug from "./TransferPlug.svelte";
+  import {
     Header,
     HeaderNav,
     HeaderNavItem,
@@ -19,58 +20,58 @@
     Row,
     Column,
   } from "carbon-components-svelte";
-  
+  // import { Theme } from "carbon-components-svelte";
+
   let isSideNavOpen = false;
 </script>
 
-
-<Header style="background-color: white; color: black;" expansionBreakpoint={1700} company="Ecofound" platformName="ICP" bind:isSideNavOpen>
-    <svelte:fragment slot="skip-to-content">
-      <SkipToContent />
-    </svelte:fragment>
-    <HeaderNav style="background-color: white;">
-      <HeaderNavItem style="color: black;" href="/" text="About" />
-      <HeaderNavItem style="color: black;" href="/" text="Help" />
-      <HeaderNavItem style="color: black;" href="/" text="Start a project" />
-      <!-- <HeaderNavMenu text="Menu">
+<!-- <Header style="background-color: white; color: black;" expansionBreakpoint={1700} company="Ecofound" platformName="ICP" bind:isSideNavOpen> -->
+<Header
+  expansionBreakpoint={1700}
+  company="Ecofound"
+  platformName="ICP"
+  bind:isSideNavOpen
+>
+  <svelte:fragment slot="skip-to-content">
+    <SkipToContent />
+  </svelte:fragment>
+  <HeaderNav>
+    <HeaderNavItem href="/" text="About" />
+    <HeaderNavItem href="/" text="Help" />
+    <HeaderNavItem href="/" text="Start a project" />
+    <!-- <HeaderNavMenu text="Menu">
         <HeaderNavItem href="/" text="Link 1" />
         <HeaderNavItem href="/" text="Link 2" />
         <HeaderNavItem href="/" text="Link 3" />
       </HeaderNavMenu> -->
-      <HeaderNavItem style="color: black;" href="/" text="Link" />
-      <HeaderNavItem style="color: black;" href="/" text="Link" />
-      <HeaderNavItem style="color: black;" href="/" text="Link" />
-    </HeaderNav>
-  </Header>
-  
-  <SideNav bind:isOpen={isSideNavOpen}>
-    <SideNavItems>
-      <SideNavLink text="Link 1" />
-      <SideNavLink text="Link 2" />
-      <SideNavLink text="Link 3" />
-      <SideNavMenu text="Menu">
+  </HeaderNav>
+</Header>
+
+<SideNav bind:isOpen={isSideNavOpen}>
+  <SideNavItems>
+    <SideNavLink text="About" />
+    <SideNavLink text="Help" />
+    <SideNavLink text="Start a project" />
+    <!-- <SideNavMenu text="Menu">
         <SideNavMenuItem href="/" text="Link 1" />
         <SideNavMenuItem href="/" text="Link 2" />
         <SideNavMenuItem href="/" text="Link 3" />
-      </SideNavMenu>
-      <SideNavDivider />
-      <SideNavLink text="Link 4" />
-    </SideNavItems>
-  </SideNav>
-  
-  <Content>
-    <Grid>
-      <Row>
-        <Column>
-          <h1>Welcome</h1>
-        </Column>
-      </Row>
-    </Grid>
-  </Content>
+      </SideNavMenu> -->
+    <SideNavDivider />
+    <!-- <Theme render="toggle" /> -->
+  </SideNavItems>
+</SideNav>
 
-
-
+<Content>
+  <!-- <Grid>
+    <Row>
+      <Column>
+        <h1>Welcome</h1>
+      </Column>
+    </Row>
+  </Grid> -->
+  <Cards />
+</Content>
 
 <style>
-   
 </style>
