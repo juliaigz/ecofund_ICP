6f8fd9e43f8b48554b32dbdcd4418b291b40810e
estebanLoza <estebanm.hloza@gmail.com>
2024-07-31 02:09:29 -0600

    Se termina el diseño de la sección Hero, con esto se concluye el diseño de la página principal, ahora solo falta la interactividad de la página.

--- src/ecofund-icp-frontend/src/lib/Hero.svelte
+++ src/ecofund-icp-frontend/src/lib/Hero.svelte
@@ -1,12 +1,53 @@
-<script></script>
+<script>
+  
+import { AspectRatio } from "carbon-components-svelte";
 
+//importación de la imagen home.png
+import home from "$lib/images/home.png";
 
-<h1>hero </h1>
+//Importacion de Grid
+import { Grid, Row, Column } from "carbon-components-svelte";
+
+</script>
+
+<AspectRatio>
+  <Grid style="display:grid; height:100%; place-items: center;">
+    <Row>
+      <!--Columna de Texto-->
+      <Column>
+        <h2>Collective endorsement for the benefit of he enviroment</h2>
+        <p>
+          Use your ICP to endorse enviromental projects and you will receive
+          your money back with interest. Tomorrow you could also be endorsed. Join ecofund
+          and support sustainability
+        </p>
+        
+      </Column>
+      <!--Columna de Imagen-->
+      <Column>
+        <div>
+          <img src={home} alt="Imagen de bievenida, una niña en un escritorio">
+        </div>
+      </Column>
+    </Row>
+  </Grid>
+
+</AspectRatio>
+
 
 
 
 <style>
-    h1{
-        color: red;
+    img{
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      
+    }
+    .triangle-background {
+      width: 0;
+      height: 0;
+      border-left: 100px solid transparent; /* Ajusta el tamaño del triángulo */
+      border-bottom: 100px solid blue; /* Color del triángulo */
     }
 </style>
