fcef5404eebe45ccf720c85bb0cba8d8c1e0a750
santiRendon <xantirendonc@gmail.com>
2024-09-01 23:04:08 -0500

    refactor: Update Navbar component to include Cards and remove unused imports

--- src/ecofund-icp-frontend/src/lib/Navbar.svelte
+++ src/ecofund-icp-frontend/src/lib/Navbar.svelte
@@ -1,36 +1,18 @@
 <script>
-  // import { logoBurbujas } from "../../static/logoBurbujas.png";
-  import ConnectPlug from "$lib/ConnectPlug.svelte";
-  import Cards from "./Cards.svelte";
-  import TransferPlug from "./TransferPlug.svelte";
   import {
     Header,
     HeaderNav,
     HeaderNavItem,
-    HeaderNavMenu,
     SideNav,
     SideNavItems,
-    SideNavMenu,
-    SideNavMenuItem,
     SideNavLink,
     SideNavDivider,
     SkipToContent,
-    Content,
-    Grid,
-    Row,
-    Column,
   } from "carbon-components-svelte";
-  // import { Theme } from "carbon-components-svelte";
 
   let isSideNavOpen = false;
-
-  import Hero from "$lib/Hero.svelte";
-  import Features from "$lib/Features.svelte";
-  import Footer from "$lib/Footer.svelte";
-
 </script>
 
-<!-- <Header style="background-color: white; color: black;" expansionBreakpoint={1700} company="Ecofound" platformName="ICP" bind:isSideNavOpen> -->
 <Header
   expansionBreakpoint={1700}
   company="Ecofound"
@@ -44,11 +26,6 @@
     <HeaderNavItem href="/" text="About" />
     <HeaderNavItem href="/" text="Help" />
     <HeaderNavItem href="/" text="Start a project" />
-    <!-- <HeaderNavMenu text="Menu">
-        <HeaderNavItem href="/" text="Link 1" />
-        <HeaderNavItem href="/" text="Link 2" />
-        <HeaderNavItem href="/" text="Link 3" />
-      </HeaderNavMenu> -->
   </HeaderNav>
 </Header>
 
@@ -57,29 +34,10 @@
     <SideNavLink text="About" />
     <SideNavLink text="Help" />
     <SideNavLink text="Start a project" />
-    <!-- <SideNavMenu text="Menu">
-        <SideNavMenuItem href="/" text="Link 1" />
-        <SideNavMenuItem href="/" text="Link 2" />
-        <SideNavMenuItem href="/" text="Link 3" />
-      </SideNavMenu> -->
+
     <SideNavDivider />
-    <!-- <Theme render="toggle" /> -->
   </SideNavItems>
 </SideNav>
-
-<Content>
-  <!-- <Grid>
-    <Row>
-      <Column>
-        <h1>Welcome</h1>
-      </Column>
-    </Row>
-  </Grid> -->
-  <Hero />
-  <Features />
-  <Cards />
-  <Footer />
-</Content>
 
 <style>
 </style>
