352ac50477489c1da7c41d9a78b4daa7e9dcd165
santiRendon <xantirendonc@gmail.com>
2024-09-01 23:04:34 -0500

    refactor: Update +page.svelte to include Hero, Features, and Cards components

--- src/ecofund-icp-frontend/src/routes/+page.svelte
+++ src/ecofund-icp-frontend/src/routes/+page.svelte
@@ -1,11 +1,15 @@
 <script>
-  
+    import Cards from "$lib/Cards.svelte";
+    import Hero from "$lib/Hero.svelte";
+    import Features from "$lib/Features.svelte";
+    import { Content } from "carbon-components-svelte";
 </script>
 
-
-
-
+<Content>
+  <Hero />
+  <Features />
+  <Cards />
+</Content>
 
 <style>
-
 </style>
