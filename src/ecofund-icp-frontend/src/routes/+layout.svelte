fa5d2e8f9201861b7b0f371b4743fbdbeabc1b9c
santiRendon <xantirendonc@gmail.com>
2024-09-01 23:04:43 -0500

    refactor: Add Footer component to +layout.svelte

--- src/ecofund-icp-frontend/src/routes/+layout.svelte
+++ src/ecofund-icp-frontend/src/routes/+layout.svelte
@@ -1,11 +1,12 @@
 <script>
   import "carbon-components-svelte/css/all.css"; // https://github.com/carbon-design-system/carbon-components-svelte?tab=readme-ov-file
   import Navbar from "$lib/Navbar.svelte";
+  import Footer from "$lib/Footer.svelte";
 </script>
 
 <Navbar />
 <slot />
-
+<Footer />
 
 <style>
 </style>
