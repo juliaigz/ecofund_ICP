85e253e2b9e9a90a187799b94eef4078d55e0b44
SantiRendon <xantirendonc@gmail.com>
2024-10-10 20:02:33 -0500

    Added InternetIdentity component for authentication

---
+++ src/ecofund-icp-frontend/src/lib/connect/InternetIdentity.svelte
@@ -0,0 +1,86 @@
+<script lang="ts">
+  import { AuthClient } from "@dfinity/auth-client";
+  import { onMount } from "svelte";
+  import { Principal } from "@dfinity/principal";
+  import { auth, createActor } from "../store/auth";
+  import { Button } from "carbon-components-svelte";
+
+  let client: AuthClient | undefined;
+  let whoami: Promise<Principal> = $auth.actor.whoami(); // whoami es una promesa que resuelve en una string (principal)
+
+  onMount(async () => {
+    client = await AuthClient.create();
+    if (await client.isAuthenticated()) {
+      handleAuth();
+    }
+  });
+
+  function handleAuth() {
+    if (!client) return; // Verificamos que client no sea undefined
+    auth.update(() => ({
+      loggedIn: true,
+      actor: createActor({
+        agentOptions: {
+          identity: client!.getIdentity(),
+        },
+      }),
+    }));
+
+    whoami = $auth.actor.whoami();
+  }
+
+  function login() {
+    if (!client) return; // Verificamos que client no sea undefined
+    client.login({
+      identityProvider:
+        process.env.DFX_NETWORK === "ic"
+          ? "https://identity.ic0.app/#authorize"
+          : `http://${process.env.CANISTER_ID_INTERNET_IDENTITY}.localhost:4943/#authorize`,
+      onSuccess: handleAuth,
+    });
+  }
+
+  async function logout() {
+    if (!client) return; // Verificamos que client no sea undefined
+    await client.logout();
+    auth.update(() => ({
+      loggedIn: false,
+      actor: createActor(),
+    }));
+
+    whoami = $auth.actor.whoami();
+  }
+</script>
+
+<div class="container">
+  {#if $auth.loggedIn}
+    <div>
+      <Button on:click={logout}>Log out</Button>
+    </div>
+  {:else}
+    <Button on:click={login}>Internet Identity</Button>
+  {/if}
+
+  <div class="principal-info">
+    principal information:
+    {#await whoami}
+      Querying caller identity...
+    {:then principal}
+      Your principal ID is
+      <code>{principal}</code>
+      {#if principal.isAnonymous()}
+        (anonymous)
+      {/if}
+    {/await}
+  </div>
+</div>
+
+<style>
+  .container {
+    margin: 64px 0;
+  }
+
+  .principal-info {
+    margin-top: 32px;
+  }
+</style>
