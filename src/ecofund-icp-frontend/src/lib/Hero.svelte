657f642c8d4f6d14323caed45c00ea8885d435b2
estebanLoza <estebanm.hloza@gmail.com>
2024-11-22 16:40:40 -0600

    3 archivos son el diseño del front end principal, dado que estos son componentes

--- src/ecofund-icp-frontend/src/lib/Hero.svelte
+++ src/ecofund-icp-frontend/src/lib/Hero.svelte
@@ -6,19 +6,27 @@
 
   //Importacion de Grid
   import { Grid, Row, Column } from "carbon-components-svelte";
+  import { Button } from 'carbon-components-svelte';
+  import Add from "carbon-icons-svelte/lib/Add.svelte";
+
 </script>
 
 <AspectRatio>
   <Grid style="display:grid; height:100%; place-items: center;">
     <Row>
       <!--Columna de Texto-->
-      <Column>
-        <h2>Collective endorsement for the benefit of he enviroment</h2>
+      <Column class="TextHero">
+        <h2>Collective endorsement for the benefit of the environment</h2>
         <p>
           Use your ICP to endorse enviromental projects and you will receive
           your money back with interest. Tomorrow you could also be endorsed.
           Join ecofund and support sustainability
         </p>
+
+        <div class="BoxButton">
+          <Button style="background-color:#59CF8C;" icon={Add}>Find a project</Button>
+        </div>
+
       </Column>
 
       <!--*Columna de Imagen-->
@@ -40,4 +48,29 @@
     height: 100%;
     object-fit: cover;
   }
+
+  :global(.TextHero) h2{
+    margin-top: 6.6%;
+    font-size: 3.3rem;
+  }
+
+
+  :global(.TextHero) p{
+    margin-top: 10%;
+    font-size: 1.56em;
+  
+  }
+
+  .BoxButton{
+    margin-top: 10%;
+    position: relative;
+    left:35%;
+  }
+
+
+
+
+
+
+
 </style>
