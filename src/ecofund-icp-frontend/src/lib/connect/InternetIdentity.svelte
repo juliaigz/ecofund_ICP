d65165a4851f5c7e709a525900d367fad887acab
estebanLoza <estebanm.hloza@gmail.com>
2024-11-22 16:38:51 -0600

    estos archivos se hizo para poder hacer el diseño dle modal que esta en el incio de sesión, este muestra las wallet que se pueden conectar

--- src/ecofund-icp-frontend/src/lib/connect/InternetIdentity.svelte
+++ src/ecofund-icp-frontend/src/lib/connect/InternetIdentity.svelte
@@ -4,6 +4,7 @@
   import { Principal } from "@dfinity/principal";
   import { auth, createActor } from "../store/auth";
   import { Button } from "carbon-components-svelte";
+  import InternetComputer from "$lib/images/internet-computer-icp-seeklogo.svg";
 
   let client: AuthClient | undefined;
   let whoami: Promise<Principal> = $auth.actor.whoami(); // whoami es una promesa que resuelve en una string (principal)
@@ -62,7 +63,16 @@
       <Button on:click={logout}>Log out</Button>
     </div>
   {:else}
-    <Button on:click={login}>Internet Identity</Button>
+    <div class="PrincipalButtonIdentity">
+      <Button class="buttonIdentity" style="width: 42rem; height: 8rem;" on:click={login}>
+        Internet Identity
+        <img
+          class="internetComputer"
+          src={InternetComputer}
+          alt="Internet Computer Logo"
+        />
+      </Button>
+    </div>
   {/if}
 
   <div class="principal-info">
@@ -86,5 +96,34 @@
 
   .principal-info {
     margin-top: 32px;
+    text-align: center;
+    font-weight: bold;
+    color: rgb(27, 27, 27);
+  }
+
+  .PrincipalButtonIdentity{
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  :global(.buttonIdentity) {
+    background-color: #59cf8c;
+    width: 12rem;
+    border-radius: 7px;
+    padding: 5%;
+    display: flex; 
+    justify-content: center;
+  }
+
+  :global(.buttonIdentity):hover {
+    background-color: white;
+    border-color: #59cf8c;
+    color: black;
+  }
+
+  .internetComputer{
+    width: 6em;
   }
 </style>
