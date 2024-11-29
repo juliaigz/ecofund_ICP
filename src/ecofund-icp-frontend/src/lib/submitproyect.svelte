e2013044a6ea7b6f7cecb8fdb4b3046ec9cfa343
SantiRendon <xantirendonc@gmail.com>
2024-11-29 13:48:19 -0500

    fix unused code

--- src/ecofund-icp-frontend/src/lib/submitproyect.svelte
+++ src/ecofund-icp-frontend/src/lib/submitproyect.svelte
@@ -1,30 +1,24 @@
 <script>
-  import { ProgressIndicator, ProgressStep } from "carbon-components-svelte";
-  import { Grid, Row, Column } from "carbon-components-svelte";
-  import { TextInput } from "carbon-components-svelte";
-  import { MultiSelect } from "carbon-components-svelte";
-  import { TextArea } from "carbon-components-svelte";
-  import { FileUploader } from "carbon-components-svelte";
-  import { Button } from "carbon-components-svelte";
-  import { InlineNotification } from "carbon-components-svelte";
+  import { Grid } from "carbon-components-svelte";
   import { ToastNotification } from "carbon-components-svelte";
 </script>
 
 <Grid>
-  
   <ToastNotification class="warning" kind="warning" lowContrast>
     <strong slot="title">Important information</strong>
-    <strong slot="subtitle">We are going to deposit the funds to the wallet with which you connected in the log in with this address:</strong>
-    <strong slot="caption">mbnw5-73gpb-axml4-tfodx-tz3ch-4iyvk-qdlh6-c7nir-4a2rc-6q43y-iqe</strong> <!--Aqui va la direccion de la wallet-->
-
+    <strong slot="subtitle"
+      >We are going to deposit the funds to the wallet with which you connected
+      in the log in with this address:</strong
+    >
+    <strong slot="caption"
+      >mbnw5-73gpb-axml4-tfodx-tz3ch-4iyvk-qdlh6-c7nir-4a2rc-6q43y-iqe</strong
+    >
+    <!--Aqui va la direccion de la wallet-->
   </ToastNotification>
-
 </Grid>
+
 <style>
-  :global(.warning){
+  :global(.warning) {
     width: 100%;
   }
-
-
-
 </style>
