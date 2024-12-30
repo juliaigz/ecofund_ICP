f61f0833cdc09a4d3ab6cee9f0431d17fb2a7e5e
estebanLoza <estebanm.hloza@gmail.com>
2024-12-29 21:42:31 -0600

    refactor Hero.svelte: update class names for consistency and add responsive styles

--- src/ecofund-icp-frontend/src/lib/Hero.svelte
+++ src/ecofund-icp-frontend/src/lib/Hero.svelte
@@ -10,18 +10,18 @@
   import Add from "carbon-icons-svelte/lib/Add.svelte";
 </script>
 
-<AspectRatio>
+<!-- <AspectRatio> -->
   <Grid style="display:grid; height:100%; place-items: center;">
-    <Row>
+    <Row class="Row-Hero">
       <!--Columna de Texto-->
-      <Column class="TextHero">
+      <Column class="Text-Hero" >           
         <h2>Collective donations to benefit the environment</h2>
         <p>
           Use your ICP to support environmental projects. Tomorrow you could
           also be helped. Join Ecofund and support sustainability.
         </p>
 
-        <div class="BoxButton">
+        <div class="Box-Button">
           <Button style="background-color:#59CF8C;" icon={Add}
             >Find a project</Button
           >
@@ -39,28 +39,66 @@
       </Column>
     </Row>
   </Grid>
-</AspectRatio>
+<!-- </AspectRatio> -->
 
 <style>
+
+
+
   img {
     width: 100%;
     height: 100%;
     object-fit: cover;
   }
 
-  :global(.TextHero) h2 {
+  :global(.Text-Hero) h2 {
     margin-top: 6.6%;
     font-size: 3.3rem;
   }
 
-  :global(.TextHero) p {
+  :global(.Text-Hero) p {
     margin-top: 10%;
     font-size: 1.56em;
   }
 
-  .BoxButton {
+  .Box-Button {
     margin-top: 10%;
     position: relative;
     left: 35%;
   }
+
+
+  /*Media Queries*/
+  @media (max-width: 425px){  /*esta va a ser nuestra medida para mobile*/
+    
+  :global(.Text-Hero){
+    width: 100%;
+    /* background-color: red; */
+    /* border: blue 2px solid ; */
+  }
+
+  :global(.Text-Hero) h2{
+    font-size: 2.3rem;
+    font-weight: 450;
+    text-align: center;
+  }
+
+  :global(.Text-Hero) p{
+    font-size: 1.2rem;
+    text-align: center;
+  }
+  :global(.Row-Hero){
+    border: 2px solid green;
+    /* width: 100vw; */
+  }
+
+    img{
+      width: 25em;
+    }
+
+  }
+
+
+
+
 </style>
